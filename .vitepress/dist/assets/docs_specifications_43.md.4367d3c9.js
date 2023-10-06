3d18c0cf84a836c3b53a4378c050f3042e316a5f
JavadYakuzaa <javad.solidity.dev@gmail.com>
2023-10-06 21:11:23 +0330

    royalty section updated

--- .vitepress/dist/assets/docs_specifications_43.md.4367d3c9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as s}from"./chunks/framework.ab3d0493.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"docs/specifications/43.md","filePath":"docs/specifications/43.md"}'),c={name:"docs/specifications/43.md"};function a(o,i,n,r,p,d){return s(),t("div")}const m=e(c,[["render",a]]);export{f as __pageData,m as default};
